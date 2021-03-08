8c01f6ee1ad7ecf166b7860d573c20f2d7451c0a
dansu <daniel.sundbeg@gmail.com>
2021-03-08 22:26:14 +0100

    Whitespace format keymap

--- keyboards/handwired/funberg/split_48/keymaps/default/keymap.c
+++ keyboards/handwired/funberg/split_48/keymaps/default/keymap.c
@@ -32,34 +32,34 @@ enum planck_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
-KC_ESC    ,KC_Q      ,KC_W      ,KC_E      ,KC_R      ,KC_T                                       ,KC_Y      ,KC_U      ,KC_I      ,KC_O      ,KC_P      ,KC_BSPC   ,
-KC_TAB    ,KC_A      ,KC_S      ,KC_D      ,KC_F      ,KC_G                                       ,KC_H      ,KC_J      ,KC_K      ,KC_L      ,KC__SCLN  ,KC__QUOT  ,
-KC_LSFT   ,KC_Z      ,KC_X      ,KC_C      ,KC_V      ,KC_B                                       ,KC_N      ,KC_M      ,KC__COMM  ,KC__DOT   ,KC__SLSH  ,KC_ENT    ,
-           KC_LCTL   ,KC_LALT   ,KC_LGUI   ,RAISE     ,NAV       ,KC_BSPC              ,KC_BSPC   ,KC_SPC    ,LOWER     ,KC_RIGHT  ,KC_DOWN   ,KC_UP
+KC_ESC       ,KC_Q         ,KC_W         ,KC_E         ,KC_R         ,KC_T                                                   ,KC_Y         ,KC_U         ,KC_I         ,KC_O         ,KC_P         ,KC_BSPC      ,
+KC_TAB       ,KC_A         ,KC_S         ,KC_D         ,KC_F         ,KC_G                                                   ,KC_H         ,KC_J         ,KC_K         ,KC_L         ,KC__SCLN     ,KC__QUOT     ,
+KC_LSFT      ,KC_Z         ,KC_X         ,KC_C         ,KC_V         ,KC_B                                                   ,KC_N         ,KC_M         ,KC__COMM     ,KC__DOT      ,KC__SLSH     ,KC_ENT       ,
+              KC_LCTL      ,KC_LALT      ,KC_LGUI      ,RAISE        ,NAV          ,KC_BSPC                    ,KC_BSPC      ,KC_SPC       ,LOWER        ,KC_RIGHT     ,KC_DOWN      ,KC_UP
 ),
 [_RAISE] = LAYOUT(
-SE_ACUT   ,KC_1      ,KC_2      ,KC_3      ,KC_4      ,KC_5                                       ,KC_6      ,KC_7      ,KC_8      ,KC_9      ,KC_0      ,SE_AA     ,
-_______   ,KC_F1     ,KC_F2     ,KC_F3     ,KC_F4     ,KC_F5                                      ,XXXXXXX   ,KC_4      ,KC_5      ,KC_6      ,SE_OSLH   ,SE_ADIA   ,
-_______   ,KC_F6     ,KC_F7     ,KC_F8     ,KC_F9     ,KC_F10                                     ,XXXXXXX   ,KC_1      ,KC_2      ,KC_3      ,XXXXXXX   ,_______   ,
-           _______   ,_______   ,_______   ,_______   ,_______   ,_______              ,_______   ,_______   ,ADJUST0   ,KC_DOT    ,KC__COMM  ,XXXXXXX
+SE_ACUT      ,KC_1         ,KC_2         ,KC_3         ,KC_4         ,KC_5                                                   ,KC_6         ,KC_7         ,KC_8         ,KC_9         ,KC_0         ,SE_AA        ,
+_______      ,KC_F1        ,KC_F2        ,KC_F3        ,KC_F4        ,KC_F5                                                  ,XXXXXXX      ,KC_4         ,KC_5         ,KC_6         ,SE_OSLH      ,SE_ADIA      ,
+_______      ,KC_F6        ,KC_F7        ,KC_F8        ,KC_F9        ,KC_F10                                                 ,XXXXXXX      ,KC_1         ,KC_2         ,KC_3         ,XXXXXXX      ,_______      ,
+              _______      ,_______      ,_______      ,_______      ,_______      ,_______                    ,_______      ,_______      ,ADJUST0      ,KC_DOT       ,KC__COMM     ,XXXXXXX
 ),
 [_LOWER] = LAYOUT(
-SE_TILD   ,KC_EXLM   ,SE_AT     ,KC_HASH   ,SE_DLR    ,KC_PERC                                    ,SE_CIRC   ,SE_AMPR   ,SE_ASTR   ,SE_LPRN   ,SE_RPRN   ,SE_MINS   ,
-_______   ,XXXXXXX   ,XXXXXXX   ,SE_LCBR_MAC,SE_RCBR_MAC,XXXXXXX                                  ,XXXXXXX   ,SE_LPRN   ,SE_RPRN   ,SE_PIPE_MAC,_______  ,SE_UNDS   ,
-_______   ,XXXXXXX   ,XXXXXXX   ,SE_LBRC   ,SE_RBRC   ,XXXXXXX                                    ,XXXXXXX   ,SE_PLUS   ,SE_EQL    ,SE_BSLS_MAC,_______  ,_______   ,
-           _______   ,_______   ,_______   ,ADJUST    ,_______   ,_______              ,_______   ,_______   ,_______   ,_______   ,_______   ,_______
+SE_TILD      ,KC_EXLM      ,SE_AT        ,KC_HASH      ,SE_DLR       ,KC_PERC                                                ,SE_CIRC      ,SE_AMPR      ,SE_ASTR      ,SE_LPRN      ,SE_RPRN      ,SE_MINS      ,
+_______      ,XXXXXXX      ,XXXXXXX      ,SE_LCBR_MAC  ,SE_RCBR_MAC  ,XXXXXXX                                                ,XXXXXXX      ,SE_LPRN      ,SE_RPRN      ,SE_PIPE_MAC  ,_______      ,SE_UNDS      ,
+_______      ,XXXXXXX      ,XXXXXXX      ,SE_LBRC      ,SE_RBRC      ,XXXXXXX                                                ,XXXXXXX      ,SE_PLUS      ,SE_EQL       ,SE_BSLS_MAC  ,_______      ,_______      ,
+              _______      ,_______      ,_______      ,ADJUST       ,_______      ,_______                    ,_______      ,_______      ,_______      ,_______      ,_______      ,_______
 ),
 [_ADJUST] = LAYOUT(
-RESET     ,_______   ,_______   ,_______   ,_______   ,_______                                    ,_______   ,_______   ,_______   ,_______   ,_______   ,KC_SYSTEM_SLEEP,
-_______   ,_______   ,_______   ,_______   ,_______   ,_______                                    ,_______   ,_______   ,_______   ,_______   ,_______   ,_______   ,
-_______   ,_______   ,_______   ,MUTE      ,VOL_DOWN  ,VOL_UP                                     ,_______   ,_______   ,_______   ,_______   ,_______   ,_______   ,
-           _______   ,_______   ,_______   ,_______   ,_______   ,_______              ,_______   ,_______   ,_______   ,_______   ,_______   ,_______
+RESET        ,_______      ,_______      ,_______      ,_______      ,_______                                                ,_______      ,_______      ,_______      ,_______      ,_______      ,KC_SYSTEM_SLEEP,
+_______      ,_______      ,_______      ,_______      ,_______      ,_______                                                ,_______      ,_______      ,_______      ,_______      ,_______      ,_______      ,
+_______      ,_______      ,_______      ,MUTE         ,VOL_DOWN     ,VOL_UP                                                 ,_______      ,_______      ,_______      ,_______      ,_______      ,_______      ,
+              _______      ,_______      ,_______      ,_______      ,_______      ,_______                    ,_______      ,_______      ,_______      ,_______      ,_______      ,_______
 ),
 [_NAV] = LAYOUT(
-_______   ,_______   ,_______   ,_______   ,_______   ,_______                                    ,_______   ,C(G(KC_LEFT)),KC_UP  ,C(G(KC_RGHT)),_______,KC_DELETE ,
-CTL_TAB   ,KC_LSFT   ,KC_LCTL   ,KC_LALT   ,KC_LGUI   ,_______                                    ,_______   ,KC_LEFT   ,KC_DOWN   ,KC_RIGHT  ,_______   ,_______   ,
-_______   ,_______   ,_______   ,_______   ,_______   ,_______                                    ,_______   ,G(KC_0)   ,G(S(KC_Y)),A(G(KC_0)),_______   ,_______   ,
-           _______   ,_______   ,_______   ,_______   ,_______   ,_______              ,_______   ,_______   ,_______   ,_______   ,_______   ,_______
+_______      ,_______      ,_______      ,_______      ,_______      ,_______                                                ,_______      ,C(G(KC_LEFT)),KC_UP        ,C(G(KC_RGHT)),_______      ,KC_DELETE    ,
+CTL_TAB      ,KC_LSFT      ,KC_LCTL      ,KC_LALT      ,KC_LGUI      ,_______                                                ,_______      ,KC_LEFT      ,KC_DOWN      ,KC_RIGHT     ,_______      ,_______      ,
+_______      ,_______      ,_______      ,_______      ,_______      ,_______                                                ,_______      ,G(KC_0)      ,G(S(KC_Y))   ,A(G(KC_0))   ,_______      ,_______      ,
+              _______      ,_______      ,_______      ,_______      ,_______      ,_______                    ,_______      ,_______      ,_______      ,_______      ,_______      ,_______
 ),
 
 // [_BASE] = LAYOUT(
